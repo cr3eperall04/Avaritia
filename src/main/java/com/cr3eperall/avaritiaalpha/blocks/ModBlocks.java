d697ed2015e231754e69b430f6bc063a84c3fe8f
cr3eperall <davide.michelotti04@gmail.com>
2022-01-09 19:04:53 +0100

    Initial commit

---
+++ src/main/java/com/cr3eperall/avaritiaalpha/blocks/ModBlocks.java
@@ -0,0 +1,8 @@
+package com.cr3eperall.avaritiaalpha.blocks;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+public class ModBlocks {
+    @ObjectHolder("avaritiaalpha:crystalmatrixblock")
+    public static CrystalMatrixBlock CRYSTALMATRIXBLOCK;
+}
